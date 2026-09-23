bccd9ee68d8ca61ebb3343cfe37f65370fd72a10
MesutKizilay <mesutkizilay3868@gmail.com>
2026-09-23 16:28:26 +0300

    ee

--- DockerDemo.API/Controllers/WeatherForecastController.cs
+++ DockerDemo.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DockerDemo.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezingbb"
+            "Freezingee"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
